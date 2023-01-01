99762dd024ef22067841102f3c35702e6d226ec4
Louis De Spiegelaere <louis.despiegelaere@gmail.com>
2023-01-01 16:13:32 +0100

    added readPizzas.php

---
+++ Models/readPizzas.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'connection.php';
+function readPizzas(){
+    $query = "SELECT * FROM `Pizza`";
+    $query_params = array();
+    try {
+        $stmt = getPDO()->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex){
+        die("Failed query : " . $ex->getMessage());
+    }
+    $result = $stmt->fetchall();
+    return (!empty($result)) ? $result: 'NULL';
+}
+
+var_dump(readPizzas());
